e65185647b1e396e915a5558dc2cbd9dd811c2c1
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-05 20:37:37 +0200

    Add Ford class extending Car

---
+++ src/Ford.java
@@ -0,0 +1,21 @@
+public class Ford extends Car {
+
+    public Ford(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
+}
