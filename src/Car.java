53ca10ab45fa7ca13ed5ccc64e56f1e9803ecd7a
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-05 20:32:01 +0200

    Add Mitsubishi class extending Car

--- src/Car.java
+++ src/Car.java
@@ -4,10 +4,10 @@ public class Car {
     private int cylinders;
     private boolean engine;
 
-    public Car(String name, int cylinders, boolean engine) {
+    public Car(String name, int cylinders) {
         this.name = name;
         this.cylinders = cylinders;
-        this.engine = engine;
+        this.engine = true;
         this.wheels = 4;
     }
 
@@ -27,11 +27,15 @@ public class Car {
         return engine;
     }
 
-    public void startEngine() {
-        System.out.println("Car->startEngine(): Engine is starting...");
+    public String startEngine() {
+        return "Car->startEngine(): Engine is starting...";
     }
 
-    public void accelerate(int speed) {
-        System.out.println("Car->accelerate(): Accelerating to speed " + speed);
+    public String accelerate() {
+        return "Car->accelerate(): Accelerating";
+    }
+
+    public String brake() {
+        return "Stopping the car";
     }
 }
---
+++ src/Mitsubishi.java
@@ -0,0 +1,21 @@
+public class Mitsubishi extends Car {
+
+    public Mitsubishi(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi->startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
